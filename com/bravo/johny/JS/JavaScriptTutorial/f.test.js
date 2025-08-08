07e231673c26c8c871efad3482c1e4a529e106dd
dsushanta <dasgupta.sushanta@gmail.com>
2025-08-08 12:35:59 +0530

    added tests

---
+++ com/bravo/johny/JS/JavaScriptTutorial/f.test.js
@@ -0,0 +1,9 @@
+const { f } = require('./Function');
+
+test('should return a greeting', () => {
+  expect(f('Joey', 30)).toBe('Hello Joey.. Your age is : 30');
+});
+
+test('should return a greeting with default age', () => {
+  expect(f('Joey')).toBe('Hello Joey.. Your age is : 20');
+});
